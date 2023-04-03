8b91aac64c49ac6084378b353bbfbf741a905301
bobfishkins23 <bobfishkins23@yahoo.com>
2023-04-03 16:11:27 -0400

    Added hot roller

---
+++ Content/Dusts/Cinder.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ModLoader;
+
+namespace CCMod.Content.Dusts
+{
+	public class Cinder : ModDust
+	{
+		public override void Load()
+		{
+			var screenRef = new Ref<Effect>(Mod.Assets.Request<Effect>("Assets/Effects/GlowingDust", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value);
+			Filters.Scene["GlowingDust"] = new Filter(new ScreenShaderData(screenRef, "GlowingDustPass"), EffectPriority.High);
+			Filters.Scene["GlowingDust"].Load();
+		}
+
+		public override Color? GetAlpha(Dust dust, Color lightColor)
+		{
+			Color newColor = dust.color;
+			newColor.A = 128;
+			dust.color = newColor;
+			return dust.color;
+		}
+
+		public override void OnSpawn(Dust dust)
+		{
+			dust.noGravity = true;
+			dust.scale *= 0.38f;
+			dust.frame = new Rectangle(0, 0, 160, 160);
+			dust.fadeIn = 0;
+			dust.shader = new Terraria.Graphics.Shaders.ArmorShaderData(new Ref<Effect>(Mod.Assets.Request<Effect>("Assets/Effects/GlowingDust", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value), "GlowingDustPass");
+			dust.noLightEmittence = false;
+		}
+
+		public override bool Update(Dust dust)
+		{
+			if (!dust.noLightEmittence)
+			{
+				dust.position -= Vector2.One * 80 * dust.scale;
+				dust.noLightEmittence = true;
+				dust.velocity.Y *= 1.1f;
+			}
+
+			if (dust.customData is Vector2 target)
+			{
+				Vector2 pos = dust.position + Vector2.One * 80 * dust.scale;
+				dust.velocity = Vector2.Lerp(dust.velocity, pos.DirectionTo(target) * pos.Distance(target) * 0.1f, 0.03f);
+				if (pos.Distance(target) < 3f)
+					dust.active = false;
+			}
+
+			if (dust.customData is Player player && dust.fadeIn > 0)
+			{
+				dust.position += player.velocity;
+			}
+
+			dust.shader.UseColor(dust.color * (1 - dust.fadeIn / 100f));
+
+			dust.fadeIn += 3;
+			if (dust.fadeIn > 100)
+				dust.active = false;
+
+			if (dust.noGravity)
+			{
+				if (Main.rand.NextBool())
+					dust.velocity += Main.rand.NextVector2Circular(0.2f, 0.05f);
+				dust.velocity.Y -= 0.01f;
+
+				if (dust.fadeIn > 20)
+					dust.velocity *= 0.98f;
+			}
+			else if (dust.fadeIn > 10)
+			{
+				dust.velocity.Y += 0.25f;
+				if (Collision.SolidTiles(dust.position + new Vector2(0, 22), 8, 8))
+					dust.velocity = Vector2.Zero;
+			}
+
+			dust.velocity = dust.velocity.RotatedBy(0.002f * dust.alpha * dust.velocity.Length());
+
+			dust.position += dust.velocity;
+
+			return false;
+		}
+	}
+}
---
+++ Content/Items/Weapons/Melee/HotRoller.cs
@@ -0,0 +1,168 @@
+﻿using CCMod.Common;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CCMod.Content.Items.Weapons.Melee
+{
+	internal class HotRoller : ModItem, IMadeBy
+	{
+		public string CodedBy => "Jon Arbuckle";
+
+		public string SpritedBy => "Garfield";
+
+		public override void SetDefaults()
+		{
+			Item.DamageType = DamageClass.Melee;
+			Item.damage = 62;
+			Item.useTime = 30;
+			Item.useAnimation = 30;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.noMelee = true;
+			Item.noUseGraphic = true;
+			Item.rare = ItemRarityID.Orange;
+		}
+
+		public override bool CanUseItem(Player player)
+		{
+			return player.ownedProjectileCounts[ModContent.ProjectileType<HotRollerProjectile>()] <= 0;
+		}
+
+		public override bool? UseItem(Player player)
+		{
+			Projectile.NewProjectile(Projectile.GetSource_None(), player.Center, Vector2.Zero, ModContent.ProjectileType<HotRollerProjectile>(), Item.damage, Item.knockBack, player.whoAmI);
+
+			SoundEngine.PlaySound(SoundID.Item116, player.Center);
+
+			return true;
+		}
+	}
+
+	internal class HotRollerProjectile : ModProjectile
+	{
+		public Player Owner => Main.player[Projectile.owner];
+
+		public float Timer => 60 - Projectile.timeLeft;
+
+		public override void SetStaticDefaults()
+		{
+			ProjectileID.Sets.TrailCacheLength[Projectile.type] = 20;
+			ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.friendly = true;
+			Projectile.width = 120;
+			Projectile.height = 120;
+			Projectile.timeLeft = 60;
+			Projectile.tileCollide = false;
+			Projectile.penetrate = -1;
+		}
+
+		public override void AI()
+		{
+			Owner.heldProj = Projectile.whoAmI;
+
+			Projectile.Center = Owner.Center + Vector2.UnitY * Owner.gfxOffY; //stick to the player
+
+			Projectile.rotation -= -0.2f * Owner.direction;
+
+			if (Timer < 10)
+			{
+				Owner.velocity += Vector2.Normalize(Main.MouseWorld - Owner.Center) * 3;
+
+				if (Owner.velocity.Length() > 20)
+					Owner.velocity = Vector2.Normalize(Owner.velocity) * 19.99f;
+			}
+
+			Owner.direction = Owner.velocity.X > 0 ? 1 : -1;
+
+			if (Owner.velocity.Y == 0 && Projectile.timeLeft > 10)
+				Projectile.timeLeft = 10;
+
+			float dustRot = Main.rand.NextFloat(6.28f);
+			var d = Dust.NewDustPerfect(Projectile.Center + Vector2.One.RotatedBy(dustRot) * 50f, ModContent.DustType<Dusts.Cinder>(), Vector2.UnitX.RotatedBy(dustRot + 1.57f * Owner.direction) * Main.rand.NextFloat(6, 16), Owner.direction, new Color(255, Main.rand.Next(150, 255), 150), Main.rand.NextFloat(0.5f, 1.75f));
+			d.customData = Owner;
+
+			var d2 = Dust.NewDustPerfect(Projectile.Center + Vector2.One.RotatedBy(dustRot) * 50f, ModContent.DustType<Dusts.Cinder>(), Vector2.UnitX.RotatedBy(dustRot + 1.57f * Owner.direction) * Main.rand.NextFloat(4, 8), Owner.direction, new Color(255, 100, 0), Main.rand.NextFloat(1.0f, 1.75f));
+			d2.customData = Owner;
+
+			Lighting.AddLight(Projectile.Center, new Vector3(1, 0.5f, 0.2f));
+		}
+
+		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+		{
+			for (int k = 0; k < 20; k++)
+			{
+				float dustRot = (target.Center - Owner.Center).ToRotation() + Main.rand.NextFloat(1);
+				var d2 = Dust.NewDustPerfect(Projectile.Center + Vector2.One.RotatedBy(dustRot) * 50f, ModContent.DustType<Dusts.Cinder>(), Vector2.UnitX.RotatedBy(dustRot) * Main.rand.NextFloat(4, 8), 0, new Color(255, 100, 0), Main.rand.NextFloat(1.0f, 1.75f));
+				d2.customData = Owner;
+			}
+
+			if (Projectile.timeLeft > 10)
+			{
+				Projectile.timeLeft = 10;
+				Owner.velocity *= -0.75f;
+			}
+
+			SoundEngine.PlaySound(SoundID.Item71, Owner.Center);
+			SoundEngine.PlaySound(SoundID.DD2_ExplosiveTrapExplode, Owner.Center);
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			Texture2D tex = ModContent.Request<Texture2D>("CCMod/Content/Items/Weapons/Melee/HotRollerProjectile").Value;
+			Texture2D tex2 = ModContent.Request<Texture2D>("CCMod/Content/Items/Weapons/Melee/HotRollerProjectileGlow").Value;
+			Texture2D tex3 = ModContent.Request<Texture2D>("CCMod/Content/Items/Weapons/Melee/HotRoller").Value;
+
+			float fade = 1f;
+
+			if (Timer < 10)
+				fade = Timer / 10f;
+
+			if (Timer > 50)
+				fade = 1 - (Timer - 50) / 10f;
+
+			Color color = Color.White * fade;
+			color.A = 0;
+
+			Color color2 = new Color(255, 100, 0) * MathF.Sin(Timer * 0.05f);
+			color2.A = 0;
+
+			float scale = 1.15f + MathF.Sin(Timer * 0.05f) * 0.05f;
+
+			for (int k = 0; k < Projectile.oldPos.Length; k++)
+			{
+				float progress = 1 - k / (float)Projectile.oldPos.Length;
+				Color trailColor = Color.Lerp(Color.White, new Color(255, 100, 0), 1 - progress) * progress * 0.05f * fade;
+				trailColor.A = 0;
+				Main.spriteBatch.Draw(tex2, Projectile.oldPos[k] + Projectile.Size / 2 - Main.screenPosition, null, trailColor, Projectile.oldRot[k], tex2.Size() / 2, scale, default, default);
+			}
+
+			Main.spriteBatch.Draw(tex2, Projectile.Center - Main.screenPosition, null, color2, Projectile.rotation, tex2.Size() / 2, scale, 0, 0);
+
+			Main.spriteBatch.Draw(tex, Projectile.Center - Main.screenPosition, null, Color.White * fade, Projectile.rotation, tex.Size() / 2, 1, 0, 0);
+
+			Main.spriteBatch.Draw(tex2, Projectile.Center - Main.screenPosition, null, color, Projectile.rotation, tex2.Size() / 2, 1, 0, 0);
+
+			SpriteEffects effects = Owner.direction == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+			Vector2 origin = Owner.direction == -1 ? tex3.Size() : Vector2.UnitY * tex3.Height;
+
+			Main.spriteBatch.Draw(tex3, Projectile.Center - Main.screenPosition, null, Color.White * fade, Projectile.rotation * 2, origin, 1, effects, 0);
+
+			for (int k = 0; k < 5; k++)
+			{
+				float progress = 1 - k / 5f;
+				Color trailColor2 = Color.Lerp(Color.White, new Color(255, 100, 0), 1 - progress) * progress * 0.5f * fade;
+				Main.spriteBatch.Draw(tex3, Projectile.Center - Main.screenPosition, null, trailColor2, Projectile.oldRot[k] * 2, origin, 1, effects, 0);
+			}
+
+			return false;
+		}
+	}
+}
