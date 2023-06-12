79fda62cf8bc404d8d58bfe6bf4f19b99f99929b
Xinim <LELwhyyoulookingatthis@gmail.com>
2023-06-12 09:29:49 +0700

    Bouncy example

---
+++ Common/GlobalItems/BouncyProjectile.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CCMod.Common.GlobalItems
+{
+	internal class BouncyProjectile : GlobalProjectile
+	{
+		public override void SetDefaults(Projectile entity)
+		{
+			base.SetDefaults(entity);
+			if(entity.ModProjectile is IBouncyProjectile)
+			{
+				entity.tileCollide = true;
+			}
+		}
+		public override bool OnTileCollide(Projectile projectile, Vector2 oldVelocity)
+		{
+			if (projectile.ModProjectile is IBouncyProjectile Iproj)
+			{
+				if (projectile.velocity.X != oldVelocity.X)
+				{
+					projectile.velocity.X = -oldVelocity.X * Iproj.ChangeVelocityPerBounce;
+				}
+				if (projectile.velocity.Y != oldVelocity.Y)
+				{
+					projectile.velocity.Y = -oldVelocity.Y * Iproj.ChangeVelocityPerBounce;
+				}
+				if (Iproj.BounceTime > 0)
+				{
+					Iproj.BounceTime--;
+				}
+				return false;
+			}
+			return base.OnTileCollide(projectile, oldVelocity);
+		}
+		public override void PostAI(Projectile projectile)
+		{
+			base.PostAI(projectile);
+			if (projectile.ModProjectile is IBouncyProjectile Iproj)
+			{
+				if(Iproj.BounceTime < 1)
+				{
+					projectile.Kill();
+				}
+			}
+		}
+	}
+	interface IBouncyProjectile
+	{
+		/// <summary>
+		/// Set amount of time can a projectile bounce
+		/// set -1 if you want it to bounce infinitely
+		/// </summary>
+		public int BounceTime { get; set; }
+		/// <summary>
+		/// This is to decrease the velocity of the projectile
+		/// Set this to 1 if you don't want to decrease the velocity at all
+		/// This use multiplication, no point to make this any lesser than 0
+		/// </summary>
+		public int ChangeVelocityPerBounce => 1;
+	}
+}
---
+++ Content/Items/Weapons/Example/ExampleBouncyProjectile.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using CCMod.Common.GlobalItems;
+using CCMod.Utils;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace CCMod.Content.Items.Weapons.Example
+{
+	internal class ExampleBouncyProjectile : ModProjectile, IBouncyProjectile
+	{
+		public override string Texture => CCModTool.GetVanillaTexture<Item>(ItemID.Acorn);
+		public int BounceTime { get => 10; set => BounceTime = value; }
+		public int ChangeVelocityPerBounce => 1;
+		public override void SetDefaults()
+		{
+			Projectile.width = Projectile.height = 32;
+			Projectile.friendly = true;
+			Projectile.timeLeft = 900;
+		}
+		public override void AI()
+		{
+			base.AI();
+			Projectile.rotation += MathHelper.ToRadians(20);
+		}
+	}
+	class ExampleShootBouncyProjectile : ModItem
+	{
+		public override string Texture => CCModTool.GetVanillaTexture<Item>(ItemID.Acorn);
+		public override void SetDefaults()
+		{
+		}
+	}
+}
